e5008bf2fa5c5593d89b8c9f507fbb98abb1cd23
Cipta Asfahan <42613685+AsfahanX@users.noreply.github.com>
2026-04-30 23:11:51 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,30 @@ function formatRupiah(nominal: number) {
   // });
 }
 
-function ubahKolomKeRelasi(
-  foreignKey: LibraryName,
-  column: string,
-  libraryName: LibraryName,
-  ownerKey: string,
-) {
-  lib()
-    .entries()
-    .forEach((e) => {
-      const val = e.field(column);
-      if (val) {
-        const parentLib = libByName(libraryName);
-        const parent = parentLib?.findByKey(val);
-        e.set(foreignKey, [parent]);
-      }
-    });
+function coba1() {
+  return "satu";
 }
+function coba2() {
+  return "dua";
+}
+
+// function ubahKolomKeRelasi(
+//   foreignKey: LibraryName,
+//   column: string,
+//   libraryName: LibraryName,
+//   ownerKey: string,
+// ) {
+//   lib()
+//     .entries()
+//     .forEach((e) => {
+//       const val = e.field(column);
+//       if (val) {
+//         const parentLib = libByName(libraryName);
+//         const parent = parentLib?.findByKey(val);
+//         e.set(foreignKey, [parent]);
+//       }
+//     });
+// }
 
 // function dialogKonfirmasi(callback: () => boolean | void) {
 //   dialog()
--- ts-memento.js
+++ ts-memento.js
@@ -4,13 +4,9 @@ function formatRupiah(nominal) {
   }
   return "Rp " + nominal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
-function ubahKolomKeRelasi(foreignKey, column, libraryName, ownerKey) {
-  lib().entries().forEach((e) => {
-    const val = e.field(column);
-    if (val) {
-      const parentLib = libByName(libraryName);
-      const parent = parentLib?.findByKey(val);
-      e.set(foreignKey, [parent]);
-    }
-  });
+function coba1() {
+  return "satu";
+}
+function coba2() {
+  return "dua";
 }
