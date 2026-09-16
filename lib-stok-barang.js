4dc1d7f061589f22a69562742003e801ff8158b8
Cipta Asfahan <42613685+AsfahanX@users.noreply.github.com>
2026-09-16 21:53:36 +0700

    Update lib-stok-barang.js

--- lib-stok-barang.js
+++ lib-stok-barang.js
@@ -1,120 +1,85 @@
-let gudangs = null
+const libStokBarang = {
+  helper: {
+    _gudangs: null,
+    gudangs() {
+      if (this._gudangs) return this._gudangs;
 
-function getGudangs()
-{
+      let libStokBarang = libById("RUNQRCkxQUk6JmhzOilQVjNJV28");
+      let libGudang = libById("XSNaUEFQbWdzWHBnJXVdNXZUTlE");
+      let fieldNames = libStokBarang.fields();
 
-let libStokBarang = libById(
-  'RUNQRCkxQUk6JmhzOilQVjNJV28'
-)
-let libGudang = libById(
-  'XSNaUEFQbWdzWHBnJXVdNXZUTlE'
-)
+      this._gudangs = libGudang
+        .entries()
+        .filter((v) => fieldNames.includes(v.name));
+      return this._gudangs;
+    },
 
-let fieldNames = libStokBarang
-  .fields()
+    createIfMissing(barangs) {
+      let libBarang = libById("QFQxY0BKVWQ0elJkKTY5SSU6cUM");
+      let libStokBarang = libById("RUNQRCkxQUk6JmhzOilQVjNJV28");
 
-return libGudang
-  .entries()
-  .filter(v =>
-    fieldNames.includes(v.name)
-)
+      barangs ??= libBarang.entries();
+      let entries = libStokBarang
+        .entries()
+        .map((v) => v.field("Barang")?.[0]?.id);
 
-}
+      let missings = barangs.filter((v) => !entries.includes(v.id));
 
-function createIfMissing(barangs)
-{
+     // log(missings.length);
+      return missings.map((v) =>
+        libStokBarang.create({
+          Barang: [v],
+          "Gambar utama": v.field("Gambar utama"),
+        }),
+      );
+    },
 
-let libBarang = libById(
-  'QFQxY0BKVWQ0elJkKTY5SSU6cUM'
-)
-let libStokBarang = libById(
-  'RUNQRCkxQUk6JmhzOilQVjNJV28'
-)
+    findEntries(barangs) {
+      let libBarang = libById("QFQxY0BKVWQ0elJkKTY5SSU6cUM");
+      let libStokBarang = libById("RUNQRCkxQUk6JmhzOilQVjNJV28");
 
-barangs ??= libBarang.entries()
-let entries = libStokBarang
-  .entries()
-  .map(v => v
-    .field('Barang')?.[0]?.id
-  )
+      barangs ??= libBarang.entries();
+      this.createIfMissing(barangs);
+      barangs = barangs.map((v) => v.id);
 
-let missings = barangs
-  .filter(v => !entries
-    .includes(v.id)
-  )
+      return libStokBarang
+        .entries()
+        .filter((v) => barangs.includes(v.field("Barang")?.[0]?.id));
+    },
 
-log(missings.length)
-return missings
-  .map(v => libStokBarang
-    .create({
-      Barang: [v],
-      'Gambar utama': v.field(
-        'Gambar utama'
-      )
-    })
-  )
+    updateStockBalance(e) {
+      e ??= entry();
+      let barang = e.field("Barang")?.[0] ?? undefined;
+      if (!barang) {
+        return;
+      }
 
-}
+      this.gudangs().forEach((gudang) => {
+        let result = sql(
+          'SELECT SUM(j."_Perubahan kuantitas") as total ' +
+            'FROM "Item Jurnal Barang" j ' +
+            'JOIN "Master Barang" b ' +
+            "ON j.Barang = b.id " +
+            "WHERE j.removed = 0 " +
+            `AND j.Barang = '${barang.id}' ` +
+            `AND j.Gudang = '${gudang.id}' `,
+        );
+        result = result.asInt();
+        result = result == 0 ? null : result;
 
-function findEntries(barangs)
-{
+        e.set(gudang.name, result);
+      });
+    },
 
-let libBarang = libById(
-  'QFQxY0BKVWQ0elJkKTY5SSU6cUM'
-)
-let libStokBarang = libById(
-  'RUNQRCkxQUk6JmhzOilQVjNJV28'
-)
+    updateStok(barangs) {
+     // let barangs = items.map((v) => v.field("Barang")?.[0]);
 
-barangs ??= libBarang.entries()
-createIfMissing(barangs)
-barangs = barangs.map(v => v.id)
-
-return libStokBarang
-  .entries()
-  .filter(v => barangs.includes(
-    v.field('Barang')?.[0]?.id
-  ))
-
-
-
-}
-
-function updateStok(e) {
-
-gudangs ??= getGudangs()
-
-
-e ??= entry()
-let barang = e.field('Barang')?.[0] ?? undefined
-if(!barang) {
-  return
-}
-
-gudangs.forEach(gudang => {
-
-/*
-let result = sql(`SELECT SUM(j."_Perubahan kuantitas") as total FROM "Item Jurnal Barang" j JOIN "Master Barang" b ON j.Barang = b.id WHERE j.removed = 0 AND j.Barang = '`+ barang.id + "' AND j.Gudang = '" + gudang.id + "'")
-  .asInt();
-*/
-
-let result = sql('SELECT '+
-'SUM(j."_Perubahan kuantitas") as total '+
-'FROM "Item Jurnal Barang" j '+
-'JOIN "Master Barang" b '+
-'ON j.Barang = b.id '+
-'WHERE j.removed = 0 '+
-'AND j.Barang ='+`'${barang.id}' `+
-'AND j.Gudang ='+`'${gudang.id}' `
-)
-result = result.asInt()
-result = result == 0
-  ? null
-  : result
-
-e.set(gudang.name, result)
-
-
-})
-
-}
+      this.findEntries(barangs).forEach((v) => {
+        this.updateStockBalance(v);
+        v.recalc();
+        v.field("Barang")?.[0]?.recalc();
+        message("Stok diupdate: " + v.name);
+      });
+    },
+  },
+};
